2cbb03bcd3b151e96c8afbc760d891121014a37a
Алёна Алёна <alenchaeto1@outlook.com>
2023-01-18 15:08:36 +0300

    Добавьте файлы проекта.

---
+++ Task17/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Client<T>
+    {
+        private T AccountNum { get; set; }
+        private string Name { get; set; }
+        private double Amount { get; set; }
+
+
+        public string GetInfo()
+        {
+            return $"{AccountNum} {Name} {Amount}";
+        }
+
+        public void InputInfo()
+        {
+            Console.WriteLine("Введите числовой код клиента");
+            AccountNum = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите сумму денег на счете");
+            Amount = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+
+
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client<int> client1 = new Client<int>();
+            client1.InputInfo();
+            Console.WriteLine(client1.GetInfo());
+
+            Client<string> client2 = new Client<string>();
+            client2.InputInfo();
+            Console.WriteLine(client2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+
+}
